0ad76a4e198888aefded6af4cc7a6c6fdd6ac436
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-02 06:58:28 -0400

    Implemented user registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,19 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
+  if (isValid(username)) {
+    return res.status(404).json({message: "User already exists!"});
+  }
+  users.push({"username": username, "password": password});
+  return res.status(200).json({message: "User successfully registered. Now you can login"});
 });
 
 // Get the book list available in the shop
@@ -40,7 +49,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;
