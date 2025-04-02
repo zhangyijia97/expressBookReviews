8c974b706ae25878116276412b286ce7166f163e
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-02 05:17:12 -0400

    Implemented get by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,8 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
