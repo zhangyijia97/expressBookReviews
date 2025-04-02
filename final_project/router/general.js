1a5c76113ea5918ce55a1943537eff8da55cf2a6
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-02 07:47:49 -0400

    Changed all get books methods to use Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,28 +22,48 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  let get_books = new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books,null,4));
+  });
+  get_books.then((data) => {
+    res.send(data);
+  });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  let get_book_by_isbn = new Promise ((resolve, reject) => {
+    const isbn = req.params.isbn;
+    resolve(books[isbn]);
+  });
+  get_book_by_isbn.then((data) => {
+    res.send(data);
+  });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  res.send(Object.values(books).filter((book) => book.author == author))
+  let get_book_by_author = new Promise ((resolve, reject) => {
+    const author = req.params.author;
+    resolve(Object.values(books).filter((book) => book.author == author));
+  });
+  get_book_by_author.then((data) => {
+    res.send(data);
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  res.send(Object.values(books).filter((book) => book.title == title))
+  let get_book_by_title = new Promise ((resolve, reject) => {
+    const title = req.params.title;
+    resolve(Object.values(books).filter((book) => book.title == title));
+  });
+  get_book_by_title.then((data) => {
+    res.send(data);
+  });
 });
 
 //  Get book review
